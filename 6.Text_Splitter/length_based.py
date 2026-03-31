798572240b8c753fc03c97fd2b0ea9cef1d950dc
Krishnav Talukdar <153765097+CYBERCONQUEROR@users.noreply.github.com>
2026-03-31 18:08:20 +0530

    Done Text Splitter

---
+++ 6.Text_Splitter/length_based.py
@@ -0,0 +1,21 @@
+from langchain_text_splitters import TextSplitter
+
+
+text='''
+The idea for the present invention evolved from our earlier work on real-time Indian Sign Language (ISL) 
+translation systems. During the development and testing of 
+initial prototypes, we observed that character-level recognition 
+(fingerspelling) significantly slowed down communication and was 
+not suitable for real-world conversational scenarios. While static 
+gesture recognition for alphabets and numbers achieved reasonable 
+accuracy, it resulted in unnatural and time-consuming interactions 
+when used for full sentence communication.
+'''
+
+splitter = TextSplitter(
+    chunk_size=100,
+    chunk_overlap=20,
+    separators=''
+)
+result = splitter.split_character(text)
+print(result)   
